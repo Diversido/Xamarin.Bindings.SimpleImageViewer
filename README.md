f54fef360ca472905d84423d3ecc3d618c876706
Seifer <alexey.shikov@gmail.com>
2020-05-05 17:05:39 +0300

    Fix build steps

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # Xamarin.Bindings.SimpleImageViewer
 
-There is a Xamarin bindings for SimpleImageViewer iOS library by aFrogleap
+This is a Xamarin bindings for SimpleImageViewer iOS library by aFrogleap
 https://github.com/aFrogleap/SimpleImageViewer
 
+The original iOS library can not be used as-is, and we have to make a few modifications.
+The modified library can be found here:
+https://github.com/Diversido/SimpleImageViewer/tree/modification-for-xamarin-bindings
+
+## NuGet
+
 Nuget is available: https://www.nuget.org/packages/Xamarin.Bindings.SimpleImageViewer/
+
+## Usage
 
 Usage is similar to the native one:
 ```
@@ -18,3 +26,19 @@ var imageViewController = new ImageViewerController (configuration);
 PresentViewController (imageViewController, true, null);
 ```
 
+# Contribution Guide
+
+## Prepare
+
+1. Ensure `sharpie` installed: https://docs.microsoft.com/en-us/xamarin/cross-platform/macios/binding/objective-sharpie/get-started
+
+2. Open `build.sh` and update `IOS_SDK` property to match system SDK
+
+## Run
+
+`sh build.sh`
+
+## Check
+
+1. Ensure DLL is created
+2. Ensure nuget files are created
--- build.sh
+++ build.sh
@@ -1,21 +1,36 @@
+#!/bin/bash
+# TODO
+# 1. Ensure to use the latest sharpie (the latest version that worked is 3.4.0)
+# 2. Use the SDK version installed on your machine (the latest version that worked 13.2)
+IOS_SDK=iphoneos13.4
+
+# Build native library
 cd SimpleImageViewer
-xcodebuild -target SimpleImageViewer -scheme SimpleImageViewer -sdk iphoneos -derivedDataPath builds ONLY_ACTIVE_ARCH=NO
-xcodebuild -target SimpleImageViewer -scheme SimpleImageViewer -sdk iphonesimulator -derivedDataPath builds ONLY_ACTIVE_ARCH=NO
+xcodebuild -target SimpleImageViewer -scheme SimpleImageViewer -configuration Release -sdk iphoneos -derivedDataPath builds ONLY_ACTIVE_ARCH=NO
+xcodebuild -target SimpleImageViewer -scheme SimpleImageViewer -configuration Release -sdk iphonesimulator -derivedDataPath builds ONLY_ACTIVE_ARCH=NO
 
 cp -r builds/Build/Products/Release-iphonesimulator/SimpleImageViewer.framework/Modules/SimpleImageViewer.swiftmodule/. builds/Build/Products/Release-iphoneos/SimpleImageViewer.framework/Modules/SimpleImageViewer.swiftmodule/
 
+# Create fat library
 lipo -create -output SimpleImageViewer2 builds/Build/Products/Release-iphoneos/SimpleImageViewer.framework/SimpleImageViewer builds/Build/Products/Release-iphonesimulator/SimpleImageViewer.framework/SimpleImageViewer
 mv SimpleImageViewer2 builds/Build/Products/Release-iphoneos/SimpleImageViewer.framework
 rm builds/Build/Products/Release-iphoneos/SimpleImageViewer.framework/SimpleImageViewer
 mv builds/Build/Products/Release-iphoneos/SimpleImageViewer.framework/SimpleImageViewer2 builds/Build/Products/Release-iphoneos/SimpleImageViewer.framework/SimpleImageViewer
-sharpie bind -sdk iphoneos13.2 -namespace Xamarin.Bindings.SimpleImageViewer -scope builds/Build/Products/Release-iphoneos/SimpleImageViewer.framework/Headers builds/Build/Products/Release-iphoneos/SimpleImageViewer.framework/Headers/SimpleImageViewer-Swift.h
 
+# Sharpify
+sharpie bind -sdk $IOS_SDK -namespace Xamarin.Bindings.SimpleImageViewer -scope builds/Build/Products/Release-iphoneos/SimpleImageViewer.framework/Headers builds/Build/Products/Release-iphoneos/SimpleImageViewer.framework/Headers/SimpleImageViewer-Swift.h
+
+# Build Xamarin library
 cd ..
-nuget restore
+#nuget restore
 msbuild /t:Rebuild /p:Configuration=Release Xamarin.Bindings.SimpleImageViewer/Xamarin.Bindings.SimpleImageViewer.csproj
 
+# Prepare nuget files
 mkdir -p _builds/SimpleImageViewer
 cp Xamarin.Bindings.SimpleImageViewer/bin/Release/SimpleImageViewer*.dll _builds/SimpleImageViewer/
 mv Xamarin.Bindings.SimpleImageViewer/bin/Release/Xamarin.Bindings.SimpleImageViewer.*.nupkg Xamarin.Bindings.SimpleImageViewer/bin/Release/Xamarin.Bindings.SimpleImageViewer.nupkg
 cp Xamarin.Bindings.SimpleImageViewer/bin/Release/*.nupkg _builds/nugets
 cp Xamarin.Bindings.SimpleImageViewer/bin/Release/*.nuspec _builds/nugets
+
+# Verify
+lipo -info SimpleImageViewer/builds/Build/Products/Release-iphoneos/SimpleImageViewer.framework/SimpleImageViewer
